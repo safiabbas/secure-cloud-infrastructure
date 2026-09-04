f2c08e4a9ed10448d99f82aafdf21773d269c5ec
Safi Abbas <SafiAbbas0495@gmail.com>
2026-09-04 02:47:05 -0400

    Implement least-privilege security group rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,3 +69,92 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_security_group" "public" {
+  name        = "secure-cloud-public-sg"
+  description = "Security group for public-facing resources"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "secure-cloud-public-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "public_https" {
+  security_group_id = aws_security_group.public.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+
+  description = "Allow HTTPS from the internet"
+}
+
+resource "aws_security_group" "app" {
+  name        = "secure-cloud-app-sg"
+  description = "Security group for application resources"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "secure-cloud-app-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "app_from_public" {
+  security_group_id = aws_security_group.app.id
+
+  referenced_security_group_id = aws_security_group.public.id
+
+  from_port   = 8080
+  ip_protocol = "tcp"
+  to_port     = 8080
+
+  description = "Allow application traffic from public tier"
+}
+
+resource "aws_security_group" "data" {
+  name        = "secure-cloud-data-sg"
+  description = "Security group for database resources"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "secure-cloud-data-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "data_from_app" {
+  security_group_id = aws_security_group.data.id
+
+  referenced_security_group_id = aws_security_group.app.id
+
+  from_port   = 5432
+  ip_protocol = "tcp"
+  to_port     = 5432
+
+  description = "Allow PostgreSQL traffic from application tier"
+}
+
+resource "aws_vpc_security_group_egress_rule" "public_to_app" {
+  security_group_id = aws_security_group.public.id
+
+  referenced_security_group_id = aws_security_group.app.id
+
+  from_port   = 8080
+  ip_protocol = "tcp"
+  to_port     = 8080
+
+  description = "Allow application traffic to application tier"
+}
+
+resource "aws_vpc_security_group_egress_rule" "app_to_data" {
+  security_group_id = aws_security_group.app.id
+
+  referenced_security_group_id = aws_security_group.data.id
+
+  from_port   = 5432
+  ip_protocol = "tcp"
+  to_port     = 5432
+
+  description = "Allow PostgreSQL traffic to data tier"
+}
