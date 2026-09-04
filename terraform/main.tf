c57d742c45ce189c3855f612aadce1963bc933fe
Safi Abbas <SafiAbbas0495@gmail.com>
2026-09-04 02:21:50 -0400

    Build segmented AWS network architecture

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,62 @@ resource "aws_vpc" "main" {
     ManagedBy   = "terraform"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "secure-cloud-public-subnet"
+    Tier = "public"
+  }
+}
+
+resource "aws_subnet" "app" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "secure-cloud-app-subnet"
+    Tier = "application"
+  }
+}
+
+resource "aws_subnet" "data" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "secure-cloud-data-subnet"
+    Tier = "data"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "secure-cloud-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "secure-cloud-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,23 @@ output "vpc_cidr" {
   description = "CIDR block of the project VPC"
   value       = aws_vpc.main.cidr_block
 }
+
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+output "app_subnet_id" {
+  description = "ID of the private application subnet"
+  value       = aws_subnet.app.id
+}
+
+output "data_subnet_id" {
+  description = "ID of the private data subnet"
+  value       = aws_subnet.data.id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the VPC Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
